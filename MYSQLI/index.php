564b1b8f07670b4198dadfb670da030288aebb2c
phdev182 <phdev182@gmail.com>
2020-06-30 11:31:53 -0400

    Criando bando de dados

---
+++ MYSQLI/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Class Crud</title>
+</head>
+<body>
+  <?php
+      include("Class/ClassConexao.php");
+
+      ?>
+</body>
+</html>
---
+++ PDO/index.php
@@ -0,0 +1,10 @@
+<?php include("Includes/Header.php"); ?>
+
+<div class="Content">
+  <?php
+  include("Class/ClassConexao.php");
+  /*teste de conexão
+    $Conexao=new ClassConexao();
+    var_dump($Conexao->conectaDB());*/
+  ?>
+</div>
